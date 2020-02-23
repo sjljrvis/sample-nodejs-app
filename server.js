44b7dc31edcba446af9e1c3221e2c99205536372
sjljrvis <sjlchougule@gmail.com>
2020-02-24 01:44:29 +0530

    Tesing upweb

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-
-
 var http = require('http')
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 var userName = __dirname.trim()
 
 var server = http.createServer(function (request, response) {
         response.writeHead(200, { 'Content-Type': 'text/plain' })
-        response.end('Testing github hook !!! \n')
+        response.end('Hey this is a sample nodeJS application !!! \n')
 });
 
 server.listen(port);
+console.log(process.env.API_KEY)
 
 console.log('Server running at http://localhost:' + port + userName);
 
